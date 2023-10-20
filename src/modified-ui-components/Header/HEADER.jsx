b311ca89b6203262efa459436e4faf5db1abb0a2
Davis <dpjankevics@gmail.com>
2023-10-20 16:15:32 +0300

    Fixed header spacing

--- src/modified-ui-components/Header/HEADER.jsx
+++ src/modified-ui-components/Header/HEADER.jsx
@@ -5,16 +5,18 @@ import React from "react";
 
 export const Navbar = ({signOut }) => {
   return (
-    <div className="w-[100%] h-[4vh] relative bg-amber-50">
-      <div className="w-[100%] h-[60px] left-0 top-0 absolute bg-stone-50 border-b border-black bg-inherit" />
-      <div className="w-[20%] h-[19px] left-[14%] top-[40%] absolute text-black text-lg font-normal font-['SchoolBook'] tracking-tight"><a href="/">Sākums</a></div>
-      <div className="w-[20%] h-[19px] left-[28%] top-[40%] absolute text-black text-lg font-normal font-['SchoolBook'] tracking-tight"><a href="/search">Meklēt</a></div>
-      <div className="left-[42%] top-[12px] absolute text-black text-2xl font-bold font-['SchoolBook'] uppercase tracking-[9px]">Baltic Shorts</div>
-      <div className="w-[12%] h-[19px] right-[30%] top-[40%] text-lg absolute text-right text-black font-normal font-['SchoolBook'] tracking-tight">EN | LV | LT | EE</div>
-      <div className="w-[12%] h-[19px] right-[15%] top-[40%] text-lg absolute text-right text-black font-normal font-['SchoolBook'] tracking-tight"><a href="/profile">Mans profils</a></div>
-      <div className="w-20 h-[30px] relative">
-        <div className="w-20 h-[30px] left-0 top-0 absolute bg-stone-50 rounded-[20px] border border-black" />
-        <div className="left-[8px] top-[6px] absolute text-center text-black text-base font-normal font-['SchoolBook']" onClick={() => signOut()}>SignOut</div>
+    <div className="w-full h-[4vh] relative bg-amber-50 flex flex-row justify-center items-center">
+      <div className="w-full h-[60px] left-0 top-0 absolute bg-stone-50 border-b border-black bg-inherit" />
+      <div className="w-full flex flex-row">
+        <div className="h-[19px] m-auto pt-1 relative text-black text-lg font-normal font-['SchoolBook'] tracking-tight"><a href="/">Sākums</a></div>
+        <div className="h-[19px] m-auto pt-1 relative text-black text-lg font-normal font-['SchoolBook'] tracking-tight"><a href="/search">Meklēt</a></div>
+        <div className="m-auto pt-3 relative text-black text-2xl font-bold font-['SchoolBook'] uppercase tracking-[9px]">Baltic Shorts</div>
+        {/* <div className="w-[12%] h-[19px] right-[30%] top-[40%] text-lg absolute text-right text-black font-normal font-['SchoolBook'] tracking-tight">EN | LV | LT | EE</div> */}
+        <div className="h-[19px] m-auto pt-1 text-lg relative text-right text-black font-normal font-['SchoolBook'] tracking-tight"><a href="/profile">Mans profils</a></div>
+        <div className="h-[30px] relative m-auto pt-1">
+          <div className="w-20 h-[30px] right-0 top-0 absolute bg-stone-50 rounded-[20px] border border-black" />
+          <div className="right-[8px] top-[6px] absolute text-center text-black text-base font-normal font-['SchoolBook']" onClick={() => signOut()}>SignOut</div>
+        </div>
       </div>
     </div>
   );
