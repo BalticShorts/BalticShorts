8541c63829e1f7c7c60bbced5c24ee8583ebe8f4
Davis <dpjankevics@gmail.com>
2024-02-14 11:22:49 +0200

    Prod setup test

---
+++ src/pages/Prod.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Prod = () => {
+  return (
+    <>
+    <div className="flex justify-center items-center flex-col m-auto">
+        <h1 className="size-auto m-auto">Šeit kaut kas top!</h1>
+    </div>
+    </>
+  );
+}
+  
+  export default Prod;
