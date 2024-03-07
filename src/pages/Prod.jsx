ce5dadbe70cbf9d7d5904a9a49ec8a80fe05be74
Davis <dpjankevics@gmail.com>
2024-03-07 14:33:39 +0200

    prod fixes

--- src/pages/Prod.jsx
+++ src/pages/Prod.jsx
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from './static/logo_small.svg';
+// import video_src from './static/test.mp4';
 import video_src from './static/prod_vid.mp4';
+import mobileBackground from './static/mobile_background.jpg';
 
 const Prod = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="w-full h-screen overflow-hidden brightness-75">
-        <video className='videoTag w-full h-screen overflow-hidden object-cover -z-10' autoPlay loop muted>
-          <source src={video_src} type="video/mp4" />
-        </video>
-      </div>
-      <div className="absolute top-4 py-5">
-        <div className="flex justify-center items-center">
-          <img src={logo} alt="logo" />
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="w-full h-screen overflow-hidden brightness-75">
+          {isMobile ? (
+            <img src={mobileBackground} alt="Mobile Background" className="w-full h-screen object-cover -z-10" />
+          ) : (
+            <video className='videoTag w-full h-screen overflow-hidden object-cover -z-10' autoPlay loop muted>
+              <source src={video_src} type="video/mp4" />
+            </video>
+          )}
+        </div>
+        <div className="absolute top-2.5">
+          <div className="flex justify-center items-center">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+        <div className="absolute justify-center items-center h-screen flex flex-col select-none">
+          <div className="py-5 text-center">
+            <div className={`font-bold text-white ${isMobile ? 'text-3xl' : 'text-4xl'} uppercase font-['SchoolBook']`}>
+              Šeit no jauna top{isMobile && <br/>} Baltijas īsfilmu<br/></div>
+            <div className={`font-bold text-white ${isMobile ? 'text-3xl' : 'text-4xl'} uppercase font-['SchoolBook']`}>
+              straumēšanas{isMobile && <br/>} platforma</div>
+          </div>
+          <div className={`text-center text-white ${isMobile ? 'text-lg' : 'text-xl'} p-2 font-['SchoolBook']`}>
+            A streaming platform for Baltic short films{isMobile && <br/>} is going to be here soon
+          </div>
         </div>
       </div>
-      <div className="absolute justify-center items-center h-screen flex flex-col">
-        <div className="py-5 text-center">
-          <div className="font-bold text-white text-5xl uppercase font-['SchoolBook']">Šeit no jauna top Baltijas īsfilmu<br/></div>
-          <div className="font-bold text-white text-5xl uppercase font-['SchoolBook']">straumēšanas platforma</div>
-        </div>
-        <div className="text-center text-white text-xl p-2 font-['SchoolBook']">A streaming platform for Baltic short films is going to be here soon</div>
-      </div>
-    </div>
     </>
   );
 }
-  
-  export default Prod;
+
+export default Prod;
