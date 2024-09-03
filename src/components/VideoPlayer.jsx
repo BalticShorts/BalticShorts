939b3726d0136a2f0e7b647f79ffd304b8402b30
Davis <dpjankevics@gmail.com>
2024-09-03 11:09:14 +0300

    safari use only hls, others only dash

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,84 +1,5 @@
-// import React, { useEffect, useRef } from 'react';
-// import videojs from 'video.js';
-// import 'video.js/dist/video-js.css';
-// import 'videojs-contrib-quality-levels';
-// import 'videojs-hls-quality-selector';
-// import 'videojs-contrib-dash';
-// import {isSafari} from 'react-device-detect';
-
-// const VideoPlayer = ({ movieURL, subtitles, thumbnail }) => {
-
-//   const videoNode = useRef(null);
-//   const player = useRef(null);
-
-//   useEffect(() => {
-//     videojs.options.autoSetup = false;
-
-//     if (movieURL !== '' && movieURL !== undefined) {
-//       const videoJsOptions = {
-//         autoplay: false,
-//         preload: "auto",
-//         controls: true,
-//         poster: thumbnail,
-//         sources: [
-//           {
-//             src: movieURL,
-//             type: isSafari ? "application/x-mpegURL" : "application/dash+xml"  
-//           }
-//         ],
-//         tracks: [{src: subtitles, kind:'captions', srclang: 'en', label: 'English'}],
-//         html5: {
-//           vhs: {
-//             withCredentials: true,
-//             overrideNative: true
-//           }
-//         }
-//       };
-//       if(!isSafari){
-//         videojs.Html5DashJS.hook('beforeinitialize', function(player, media_player) {
-//           function loader() {
-//             var load = this.parent.load
-//             return {
-//               load: function(req) {
-//                 req.withCredentials = true
-//                 load(req)
-//             }}
-//           }
-//           media_player.extend('XHRLoader', loader, true)
-//           media_player.setXHRWithCredentialsForType('GET',true)
-//           media_player.setXHRWithCredentialsForType('MPD',true)
-//           media_player.setXHRWithCredentialsForType('MediaSegment',true)
-//           media_player.setXHRWithCredentialsForType('InitializationSegment',true)
-//           media_player.setXHRWithCredentialsForType('IndexSegment',true)
-//           media_player.setXHRWithCredentialsForType('other',true)
-//         })
-//       }
-
-//       player.current = videojs(videoNode.current, videoJsOptions, function onPlayerReady() {
-//         this.qualityLevels();
-//         this.hlsQualitySelector({ displayCurrentQuality: true });
-//       });
-//     }
-//     return () => {
-//       if (player.current) {
-//         player.current.dispose();
-//       }
-//     };
-
-//   }, [movieURL]);
-
-//   return (
-//     <div className='top-10'>
-//       <video id='video-player' ref={videoNode} className="video-js vjs-default-skin w-full h-[80vh] object-fill" />
-//     </div>
-//   );
-// };
-
-
-// export default VideoPlayer;
-
-
 import React, { useEffect, useRef } from 'react';
+import {isSafari} from 'react-device-detect';
 
 const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
   const playerRef = useRef(null);
@@ -127,9 +48,9 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
 
         // Load the source
         const source = {
-          dash: movieURL.dash, // DASH URL
-          hls: movieURL.hls, // HLS URL
-          poster: thumbnail, // Poster image
+          dash: !isSafari ? movieURL.dash : null, // DASH URL
+          hls: isSafari ? movieURL.hls : null, // HLS URL
+          poster: thumbnail,
           subtitle: subtitles ? {
             url: subtitles,
             kind: 'subtitles',
