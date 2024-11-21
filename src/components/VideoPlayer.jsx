f5f5d7585cd3ba371b8b756597de75373ab0a7fb
DavisJankevics <dpjnakevics@gmail.com>
2024-11-21 19:57:49 +0200

    reenable fairplay

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -56,14 +56,34 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
         console.log(movieURL)
         const source = {
           dash: movieURL.dash,
-          hls: movieURL.cmafHls,
+          hls: movieURL.hls,
           poster: thumbnail,
           drm: {
             widevine: {
                 LA_URL: 'https://e40ff278.drm-widevine-licensing.axprod.net/AcquireLicense',
                 headers: {'X-AxDRM-Message': urlAddon},
             },
-            
+            fairplay: {
+              LA_URL: 'https://e40ff278.drm-fairplay-licensing.axprod.net/AcquireLicense',
+              certificateURL: 'https://vtb.axinom.com/FPScert/fairplay.cer',
+              headers: {
+                'X-AxDRM-Message': urlAddon
+              },
+              prepareContentId: (uri) => {
+                  return uri.substring(uri.indexOf("skd"));
+                  },
+              prepareLicenseAsync: ckc => {
+                  return new Promise((resolve, reject) => {
+                      const reader = new FileReader();
+                      reader.addEventListener('loadend', () => resolve(new Uint8Array(reader.result)));
+                      reader.addEventListener('error', () => reject(reader.error));
+                      reader.readAsArrayBuffer(ckc);
+                      });
+                  },
+              prepareMessage: event => new Blob([event.message], {type: 'application/octet-binary'}),
+              useUint16InitData: true,
+              licenseResponseType: 'blob'
+              },
           },
           subtitle: subtitles ? {
             url: subtitles,
@@ -105,28 +125,6 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
 
 export default SimpleBitmovinPlayer;
 export const isVideoPlaying = video => !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
-
-// fairplay: {
-//   LA_URL: 'https://e40ff278.drm-fairplay-licensing.axprod.net/AcquireLicense',
-//   certificateURL: 'https://vtb.axinom.com/FPScert/fairplay.cer',
-//   headers: {
-//     'X-AxDRM-Message': urlAddon
-//   },
-//   prepareContentId: (uri) => {
-//       return uri.substring(uri.indexOf("skd"));
-//       },
-//   prepareLicenseAsync: ckc => {
-//       return new Promise((resolve, reject) => {
-//           const reader = new FileReader();
-//           reader.addEventListener('loadend', () => resolve(new Uint8Array(reader.result)));
-//           reader.addEventListener('error', () => reject(reader.error));
-//           reader.readAsArrayBuffer(ckc);
-//           });
-//       },
-//   prepareMessage: event => new Blob([event.message], {type: 'application/octet-binary'}),
-//   useUint16InitData: true,
-//   licenseResponseType: 'blob'
-//   },
 
 
 // options: {
