bd7b2e20018bd5ec5a0247e35a913247139b5741
DavisJankevics <dpjnakevics@gmail.com>
2024-11-21 19:00:13 +0200

    try cmaf

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -6,12 +6,12 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
 
   useEffect(() => {
     const loadPlayer = async () => {
-      const keyValuePairs = urlAddon.substring(1).split("&");
+      // const keyValuePairs = urlAddon.substring(1).split("&");
       const params = {};
-      keyValuePairs.forEach(pair => {
-          const [key, value] = pair.split("=");
-          params[key] = decodeURIComponent(value);
-      });
+      // keyValuePairs.forEach(pair => {
+      //     const [key, value] = pair.split("=");
+      //     params[key] = decodeURIComponent(value);
+      // });
 
       if(movieURL == undefined || (movieURL?.dash == undefined && movieURL?.hls == undefined)) return;
       try {
@@ -56,34 +56,14 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
         console.log(movieURL)
         const source = {
           dash: movieURL.dash,
-          hls: movieURL.hls,
+          hls: movieURL.cmafHls,
           poster: thumbnail,
           drm: {
             widevine: {
                 LA_URL: 'https://e40ff278.drm-widevine-licensing.axprod.net/AcquireLicense',
                 headers: {'X-AxDRM-Message': urlAddon},
             },
-            fairplay: {
-              LA_URL: 'https://e40ff278.drm-fairplay-licensing.axprod.net/AcquireLicense',
-              certificateURL: 'https://vtb.axinom.com/FPScert/fairplay.cer',
-              headers: {
-                'X-AxDRM-Message': urlAddon
-              },
-              prepareContentId: (uri) => {
-                  return uri.substring(uri.indexOf("skd"));
-                  },
-              prepareLicenseAsync: ckc => {
-                  return new Promise((resolve, reject) => {
-                      const reader = new FileReader();
-                      reader.addEventListener('loadend', () => resolve(new Uint8Array(reader.result)));
-                      reader.addEventListener('error', () => reject(reader.error));
-                      reader.readAsArrayBuffer(ckc);
-                      });
-                  },
-              prepareMessage: event => new Blob([event.message], {type: 'application/octet-binary'}),
-              useUint16InitData: true,
-              licenseResponseType: 'blob'
-              },
+            
           },
           subtitle: subtitles ? {
             url: subtitles,
@@ -125,6 +105,29 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
 
 export default SimpleBitmovinPlayer;
 export const isVideoPlaying = video => !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
+
+// fairplay: {
+//   LA_URL: 'https://e40ff278.drm-fairplay-licensing.axprod.net/AcquireLicense',
+//   certificateURL: 'https://vtb.axinom.com/FPScert/fairplay.cer',
+//   headers: {
+//     'X-AxDRM-Message': urlAddon
+//   },
+//   prepareContentId: (uri) => {
+//       return uri.substring(uri.indexOf("skd"));
+//       },
+//   prepareLicenseAsync: ckc => {
+//       return new Promise((resolve, reject) => {
+//           const reader = new FileReader();
+//           reader.addEventListener('loadend', () => resolve(new Uint8Array(reader.result)));
+//           reader.addEventListener('error', () => reject(reader.error));
+//           reader.readAsArrayBuffer(ckc);
+//           });
+//       },
+//   prepareMessage: event => new Blob([event.message], {type: 'application/octet-binary'}),
+//   useUint16InitData: true,
+//   licenseResponseType: 'blob'
+//   },
+
 
 // options: {
 //   withCredentials: true,
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -37,7 +37,11 @@ const fetchVideo = async guid => {
     config.aws_api_gateway + 'movies/' + guid,
     requestOptions
   ).then((response) => response.json());
-  const resp = {"hls": data.Item.hlsUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'), "dash" : data.Item.dashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'), "keyId" : data.Item.keyId?.S, "resourceId" : data.Item.resourceId?.S};
+  const resp = {"hls": data.Item.hlsUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
+    "dash" : data.Item.dashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
+    "cmafDash" : data.Item.cmafDashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
+    "cmafHls" : data.Item.cmafHlsUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
+    "keyId" : data.Item.keyId?.S, "resourceId" : data.Item.resourceId?.S};
   return resp !== undefined ? resp : '';
 }
 
