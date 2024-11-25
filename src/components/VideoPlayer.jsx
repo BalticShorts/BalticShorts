5bc55e7d836ece4763079a108878e679dff5bc03
DavisJankevics <dpjnakevics@gmail.com>
2024-11-25 19:39:46 +0200

    add tweaks and adaptation

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -52,6 +52,17 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
           logs: {
             level: 'debug', // Enable verbose logging
           },
+          tweaks: {
+            native_hls_parsing: false
+          },
+          adaptation: {
+            desktop: {
+              preload: false
+            },
+            mobile: {
+              preload: false
+            }
+          },
         };
 
         console.log('Player configuration:', config);
