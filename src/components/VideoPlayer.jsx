5c7f1d2cd93cd4dd85cfa947924687f281dd32f0
DavisJankevics <dpjnakevics@gmail.com>
2024-11-25 20:58:04 +0200

    use cmaf

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -80,7 +80,7 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
 
         const source = {
           dash: movieURL.dash,
-          hls: movieURL.hls,
+          hls: movieURL.cmafHls,
           poster: thumbnail,
           drm: {
             widevine: {
