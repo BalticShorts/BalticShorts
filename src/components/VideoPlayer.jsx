81e8f4662b8d2ebdc30ae7b67dcb178a564f1ebc
DavisJankevics <dpjnakevics@gmail.com>
2024-11-25 15:12:51 +0200

    remove preload

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -45,9 +45,17 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
           logs: {
             level: 'debug'  // Enable verbose logging
           },
-          // tweaks: {
-          //   native_hls_parsing: false
-          // },
+          adaptation: {
+            desktop: {
+              preload: false
+            },
+            mobile: {
+              preload: false
+            }
+          },
+          tweaks: {
+            native_hls_parsing: false
+          },
           drm: {
             widevine: {
                 LA_URL: 'https://e40ff278.drm-widevine-licensing.axprod.net/AcquireLicense',
