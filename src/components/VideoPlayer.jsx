7b516cac9a3cc743fa765fdbb4a6d07b037b41fe
DavisJankevics <dpjnakevics@gmail.com>
2024-11-27 23:14:31 +0200

    try new cmaf

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -41,51 +41,51 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
   // const add = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJ2ZXJzaW9uIjogMSwKICAiY29tX2tleV9pZCI6ICI2OWU1NDA4OC1lOWUwLTQ1MzAtOGMxYS0xZWI2ZGNkMGQxNGUiLAogICJtZXNzYWdlIjogewogICAgInR5cGUiOiAiZW50aXRsZW1lbnRfbWVzc2FnZSIsCiAgICAidmVyc2lvbiI6IDIsCiAgICAibGljZW5zZSI6IHsKICAgICAgImFsbG93X3BlcnNpc3RlbmNlIjogdHJ1ZQogICAgfSwKICAgICJjb250ZW50X2tleXNfc291cmNlIjogewogICAgICAiaW5saW5lIjogWwogICAgICAgIHsKICAgICAgICAgICJpZCI6ICJhYmNjNDRlNS1jMTIyLTQ1YWItYWM4MC1hNWIzNTIyYTBhMzEiLAogICAgICAgICAgImVuY3J5cHRlZF9rZXkiOiAiZnM2VUx1UzR3SFQxdkI2M0RONnI5UT09IiwKICAgICAgICAgICJ1c2FnZV9wb2xpY3kiOiAiUG9saWN5IEEiCiAgICAgICAgfQogICAgICBdCiAgICB9LAogICAgImNvbnRlbnRfa2V5X3VzYWdlX3BvbGljaWVzIjogWwogICAgICB7CiAgICAgICAgIm5hbWUiOiAiUG9saWN5IEEiLAogICAgICAgICJwbGF5cmVhZHkiOiB7CiAgICAgICAgICAibWluX2RldmljZV9zZWN1cml0eV9sZXZlbCI6IDE1MCwKICAgICAgICAgICJwbGF5X2VuYWJsZXJzIjogWwogICAgICAgICAgICAiNzg2NjI3RDgtQzJBNi00NEJFLThGODgtMDhBRTI1NUIwMUE3IgogICAgICAgICAgXQogICAgICAgIH0KICAgICAgfQogICAgXQogIH0KfQ.5rM_qUo4dKrHNDKQO0yzbCiufJxFUzHeOQc13Z48rv4'
   const sourceConfig = {
     dash: movieURL?.dash,
-    hls: 'https://vod.balticshorts.com/clear_cmaf_1080p_h265/manifest.m3u8',
+    // hls: 'https://vod.balticshorts.com/clear_cmaf_1080p_h265/manifest.m3u8',
     // hls: 'https://vod.balticshorts.com/protected_cmaf_1080p_h265_multikey/manifest.m3u8',
     // hls: 'https://vod.balticshorts.com/protected_hls_1080p_h265_singlekey/manifest.m3u8',
-    // hls: movieURL?.hls,
+    hls: movieURL?.cmafHls,
     poster: thumbnail,
-    // drm: {
-    //   widevine: {
-    //     LA_URL: "https://e40ff278.drm-widevine-licensing.axprod.net/AcquireLicense",
-    //     headers: {
-    //       "X-AxDRM-Message": add,
-    //     },
-    //   },
-    //   fairplay: {
-    //     LA_URL: "https://e40ff278.drm-fairplay-licensing.axprod.net/AcquireLicense",
-    //     certificateURL: "https://vtb.axinom.com/FPScert/fairplay.cer",
-    //     headers: {
-    //       "X-AxDRM-Message": add,
-    //     },
-    //     prepareContentId: (uri) => {
-    //       console.log("FairPlay prepareContentId called with URI:", uri);
-    //       return uri.substring(uri.indexOf("skd"));
-    //     },
-    //     prepareLicenseAsync: (ckc) => {
-    //       console.log("FairPlay prepareLicenseAsync called with CKC:", ckc);
-    //       return new Promise((resolve, reject) => {
-    //         const reader = new FileReader();
-    //         reader.addEventListener("loadend", () => {
-    //           console.log("FairPlay CKC processing complete.");
-    //           resolve(new Uint8Array(reader.result));
-    //         });
-    //         reader.addEventListener("error", (error) => {
-    //           console.error("FairPlay CKC processing failed:", error);
-    //           reject(error);
-    //         });
-    //         reader.readAsArrayBuffer(ckc);
-    //       });
-    //     },
-    //     prepareMessage: (event) => {
-    //       console.log("FairPlay prepareMessage called with event:", event);
-    //       return new Blob([event.message], { type: "application/octet-binary" });
-    //     },
-    //     useUint16InitData: true,
-    //     licenseResponseType: "blob",
-    //   },
-    // },
+    drm: {
+      widevine: {
+        LA_URL: "https://e40ff278.drm-widevine-licensing.axprod.net/AcquireLicense",
+        headers: {
+          "X-AxDRM-Message": add,
+        },
+      },
+      fairplay: {
+        LA_URL: "https://e40ff278.drm-fairplay-licensing.axprod.net/AcquireLicense",
+        certificateURL: "https://vtb.axinom.com/FPScert/fairplay.cer",
+        headers: {
+          "X-AxDRM-Message": add,
+        },
+        prepareContentId: (uri) => {
+          console.log("FairPlay prepareContentId called with URI:", uri);
+          return uri.substring(uri.indexOf("skd"));
+        },
+        prepareLicenseAsync: (ckc) => {
+          console.log("FairPlay prepareLicenseAsync called with CKC:", ckc);
+          return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.addEventListener("loadend", () => {
+              console.log("FairPlay CKC processing complete.");
+              resolve(new Uint8Array(reader.result));
+            });
+            reader.addEventListener("error", (error) => {
+              console.error("FairPlay CKC processing failed:", error);
+              reject(error);
+            });
+            reader.readAsArrayBuffer(ckc);
+          });
+        },
+        prepareMessage: (event) => {
+          console.log("FairPlay prepareMessage called with event:", event);
+          return new Blob([event.message], { type: "application/octet-binary" });
+        },
+        useUint16InitData: true,
+        licenseResponseType: "blob",
+      },
+    },
     subtitle: subtitles
       ? {
           url: subtitles,
