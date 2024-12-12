79d5c6a757a25fa8f5c1db95bb668d860e6eceeb
DavisJankevics <dpjnakevics@gmail.com>
2024-12-12 18:24:42 +0200

    remove mobile preload

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -191,7 +191,7 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
               preload: true
             },
             mobile: {
-              preload: true
+              preload: false
             }
           },
         };
@@ -283,7 +283,7 @@ const SimpleBitmovinPlayer = ({ movieURL, urlAddon, subtitles, thumbnail }) => {
   
         const primarySource = createSource(movieURL.dash, movieURL.cmafHls);
         const fallbackSource = createSource(
-          movieURL.cmafDash.replace(/(\d+)\.mpd$/, '$1_dash_h264.mpd'),
+          movieURL.cmafDash,
           movieURL.hls
         );
   
