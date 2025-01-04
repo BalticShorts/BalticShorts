07f647770f7cc4548fac097a2aec363c34700670
DavisJankevics <dpjnakevics@gmail.com>
2025-01-04 21:22:10 +0200

    dash url test

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -36,9 +36,12 @@ const fetchVideo = async guid => {
     config.aws_api_gateway + 'movies/' + guid,
     requestOptions
   ).then((response) => response.json());
-  const resp = {"hls": data.Item.hlsUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
+  //     "cmafDash" : data.Item.dashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com').replace(/(\d+)\.mpd$/, '$1_dash_h264.mpd'),
+
+  const resp = {
+    "hls": data.Item.hlsUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
     "dash" : data.Item.dashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
-    "cmafDash" : data.Item.dashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com').replace(/(\d+)\.mpd$/, '$1_dash_h264.mpd'),
+    "cmafDash" : data.Item.cmafDashUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
     "cmafHls" : data.Item.cmafHlsUrl?.S.replace('d3tou2oin9ei82.cloudfront.net', 'vod.balticshorts.com'),
     "keyId" : data.Item.keyId?.S, "resourceId" : data.Item.resourceId?.S
   };
